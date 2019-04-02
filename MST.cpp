b00235c433ae68b72572f2223f7f2e4a038157af
luisaras <luisa.ras97@gmail.com>
2019-04-02 12:34:16 -0300

    checking isConnected

--- MST.cpp
+++ MST.cpp
@@ -3,14 +3,15 @@
     using std::cin;
     using std::endl;
 #include <vector>
-    using std::vector;
+	using std::vector;
+#include <stack>
+	using std::stack;
 #include <cstdlib>
 #include <ctime>
 #include <climits>
 
 struct Edge {
-    int weight;
-    int dest;
+    int weight, dest;
     Edge(int w, int d) { weight = w; dest = d; }
 };
 
@@ -18,16 +19,48 @@ struct Node {
     vector<Edge> neighbors;
 };
 
+bool isConnected(Node* graph, int n) {
+	bool visited[n];
+	for (int i = 0; i < n; i++)
+		visited[i] = false;
+	stack<int> open;
+	open.push(0);
+	while(!open.empty()) {
+		int current = open.top();
+		open.pop();
+		if (visited[current])
+			continue;
+		visited[current] = true;
+		vector<Edge>& neighbors = graph[current].neighbors;
+		for (uint i = 0; i < neighbors.size(); i++) {
+			open.push(neighbors[i].dest);
+		}
+	}
+	for (int i = 0; i < n; i++)
+		if (!visited[i]) 
+			return false;
+	return true;
+}
+
 void generateGraph(Node* graph, int n, double p) {
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; i++) {
-            if (p > rand() / INT_MAX) {
-                int w = rand() % 100 + 1;
-                graph[i].neighbors.push_back(Edge(w, j));
-                graph[j].neighbors.push_back(Edge(w, i));
-            }
-        }
-    }
+	while (true) {
+		for (int i = 0; i < n; i++) {
+			for (int j = i + 1; j < n; j++) {
+				if (p > rand() / INT_MAX) {
+					int w = rand() % 100 + 1;
+					graph[i].neighbors.push_back(Edge(w, j));
+					graph[j].neighbors.push_back(Edge(w, i));
+				}
+			}
+		}
+		if (isConnected(graph, n))
+			return;
+		else {
+			for (int i = 0; i < n; i++) {
+				graph[i].neighbors.clear();
+			}
+		}
+	}
 }
 
 void test(int n, double p) {
