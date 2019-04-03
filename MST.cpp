a3625630d072cfa2c13915b8014e228f80b8eadd
luisaras <luisa.ras97@gmail.com>
2019-04-03 13:08:54 -0300

    node as set

--- MST.cpp
+++ MST.cpp
@@ -6,12 +6,14 @@
 	using std::vector;
 #include <stack>
 	using std::stack;
+#include <set>
+	using std::set;
 #include <algorithm>
     using std::sort;
 #include <cstdlib>
 #include <ctime>
 #include <climits>
-#define Node vector<int>
+#define Node set<int>
 
 // Edge
 
@@ -29,6 +31,7 @@ public:
     Node* nodes; // Each node is a list of edge indexes (edge's position in the graph's edge list)
     vector<Edge> edges; // List of edges
 
+	// Creates a random graph with n nodes and edges with probability p.
     Graph(int n, double p) {
         nodes = new Node[n];
         this->n = n;
@@ -38,8 +41,8 @@ public:
                 for (int j = i + 1; j < n; j++) {
                     if (p > rand() * 1.0 / RAND_MAX) {
                         int w = rand() % 100 + 1;
-                        nodes[i].push_back(edges.size());
-                        nodes[j].push_back(edges.size());
+                        nodes[i].insert(edges.size());
+                        nodes[j].insert(edges.size());
                         edges.push_back(Edge(w, i, j));
                     }
                 }
@@ -54,45 +57,45 @@ public:
         }
     }
 
+	// Sorts edges by weight.
     void sortByWeight() {
         sort(edges.begin(), edges.end(), compareEdges);
         for (int i = 0; i < n; i++)
             nodes[i].clear();
-        for (int e = 0; e < edges.size(); e++) {
-            nodes[edges[e].orig].push_back(e);
-            nodes[edges[e].dest].push_back(e);
+        for (int e = 0; e < (int) edges.size(); e++) {
+            nodes[edges[e].orig].insert(e);
+            nodes[edges[e].dest].insert(e);
         }
     }
 
+	// Creates an initial spanning tree.
     int* initializeTree() {
-        int* tree = new int[n - 1];
+		// Initialize "visited" flags
         bool visited[n];
         for (int i = 0; i < n; i++)
             visited[i] = false;
-        int e = 0;
+        // Initialize tree
+        int* tree = new int[n - 1];
+        int t = 0;
         stack<int> open;
         open.push(0);
         while(!open.empty()) {
             int current = open.top();
             open.pop();
-            for (int i = 0; i < nodes[current].size(); i++) {
-                int dest = edges[nodes[current][i]].dest;
-                if (!visited[dest]) {
-                    visited[dest] = true;
-                    open.push(dest);
-                    tree[e++] = nodes[current][i];
-                }
-                int orig = edges[nodes[current][i]].orig;
-                if (!visited[orig]) {
-                    visited[orig] = true;
-                    open.push(orig);
-                    tree[e++] = nodes[current][i];
+            for (auto it = nodes[current].begin(); it != nodes[current].end(); ++it) {
+				int e = *it; // Neighbor edge of current node
+				int v = current == edges[e].dest ? edges[e].orig : edges[e].dest; // Neighbor node
+                if (!visited[v]) { // Neighbor was not visited before
+                    visited[v] = true;
+                    open.push(v);
+                    tree[t++] = e; // Insert edge in the spanning tree
                 }
             }
         }
         return tree;
     }
 
+	// Calculates total cost of tree.
     inline int treeCost(int* tree) {
         int c = 0;
         for (int i = 0; i < n; i++) {
@@ -101,23 +104,30 @@ public:
         return c;
     }
 
+	// Prints each node's neighbors;
     void print() {
         for (int i = 0; i < n; i++) {
             cout << i << " -> ";
-            for (int j = 0; j < nodes[i].size(); j++)
-                cout << edges[nodes[i][j]].dest << " ";
+            for (auto it = nodes[i].begin(); it != nodes[i].end(); ++it) {
+				int e = *it; // Neighbor edge of node i
+				int v = edges[e].dest == i ? edges[e].orig : edges[e].dest; // Neighbor of node i
+                cout << v << " ";
+			}
             cout << endl;
         }
     }
 
+	// Prints tree's edges.
     void printTree(int* tree) {
         cout << edges[tree[0]].orig << "-" << edges[tree[0]].dest;
         for (int i = 1; i < n - 1; i++)
             cout << ", " << edges[tree[i]].orig << "-" << edges[tree[i]].dest;
-        cout << endl << "cost: " << treeCost(tree);
+        cout << endl << "cost: " << treeCost(tree) << endl;
     }
 
 private:
+
+	// Checks if graph is connected using DFS.
     inline bool isConnected() {
         bool visited[n];
         for (int i = 0; i < n; i++)
@@ -130,11 +140,10 @@ private:
             if (visited[current])
                 continue;
             visited[current] = true;
-            for (int i = 0; i < nodes[current].size(); i++) {
-                int dest = edges[nodes[current][i]].dest;
-                if (dest == current)
-                    dest = edges[nodes[current][i]].orig;
-                open.push(dest);
+            for (auto it = nodes[current].begin(); it != nodes[current].end(); ++it) {
+				int e = *it; // Neighbor edge of node i
+				int v = edges[e].dest == current ? edges[e].orig : edges[e].dest; // Neighbor of node i
+                open.push(v);
             }
         }
         for (int i = 0; i < n; i++)
@@ -145,6 +154,7 @@ private:
 
 };
 
+// Sets "available" flags for each edge in the graph.
 inline void availableEdges(bool* edges, int e, int* tree, int n) {
     for (int i = 0; i < e; i++)
         edges[i] = true;
@@ -152,42 +162,63 @@ inline void availableEdges(bool* edges, int e, int* tree, int n) {
         edges[tree[i]] = false;
 }
 
-inline vector<int> findCycle(Graph graph, int* tree, int e, bool* available) {
-    vector<int> cycle;
-    int root = graph.edges[e].dest;
+// Gets a set of edges in 
+inline set<int> findCycle(Graph graph, int* tree, int e, bool* available) {
+    set<int> cycle;
+    
+    //int root = graph.edges[e].dest;
     // TODO
     return cycle;
 }
 
-void firstImprovement(Graph graph, int* tree) {
-    graph.sortByWeight();
-    bool edges[graph.edges.size()];
-    availableEdges(edges, graph.edges.size(), tree, graph.n - 1);
-    int edgei;
-    Edge* bestEdge;
-    for (int e = 0; e < graph.edges.size(); e++) {
-        if (edges[e]) {
-            vector<int> cycle = findCycle(graph, tree, e, edges);
-            sort(cycle.begin(), cycle.end());
-
+// Modifies the given tree of the first variation that shows improvement.
+inline bool firstImprovement(Graph& graph, int* tree, bool* available) {
+    for (int e = 0; e < (int) graph.edges.size(); e++) {
+        if (available[e]) {
+			int cost = graph.edges[e].weight;
+            set<int> cycle = findCycle(graph, tree, e, available);
+			for (auto it = cycle.begin(); it != cycle.end(); ++it) {
+				if (graph.edges[*it].weight < cost) {
+					available[tree[e]] = true;
+					available[*it] = false;
+					tree[e] = *it;
+					return false;
+				}
+			}
         }
     }
+    return true;
 }
 
-void bestImprovement(Graph graph, int* tree) { }
+// Modifies the given tree of the variation that shows the best improvement.
+inline void bestImprovement(Graph& graph, int* tree) { 
+	// TODO
+}
 
-void test(int n, double p) {
+// Modifies tree until a local optima is found.
+inline void localSearch(Graph& graph, int* tree) {
+    bool available[graph.edges.size()];
+    availableEdges(available, graph.edges.size(), tree, graph.n - 1);
+    while(!firstImprovement(graph, tree, available));
+}
+
+inline void test(int n, double p) {
     Graph graph(n, p);
+    graph.sortByWeight();
+    graph.print();
     int* tree = graph.initializeTree();
-    firstImprovement(graph, tree);
-    //bestImprovement(graph, tree);
+    cout << endl << "Initial tree: " << endl;
     graph.printTree(tree);
+    //localSearch(graph, tree);
+    //cout << endl << "Optimal tree: " << endl;
+    //graph.printTree(tree);
 }
 
 int main() {
-    srand (time(NULL));
+    //srand (time(NULL));
+    srand(100);
 
-    test(5, 0.2);
+    test(5, 0.5);
 
     for(int i = 11; i < 10; i++) {
 
