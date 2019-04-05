3509cd674d8a5a06bb07f12f0b22c2a858b7e2d7
Luísa <luisamey@hotmail.com>
2019-04-05 01:49:09 -0300

    gsat

--- SAT.cpp
+++ SAT.cpp
@@ -10,13 +10,73 @@
     using std::vector;
 #include <string>
     using std::string;
+#include <cstdlib>
+#include <ctime>
 #define Clause vector<int>
 
 struct Formula {
     int v, c;
     Clause* clauses;
+
     ~Formula() { delete [] clauses; }
+
+    int evaluate(bool* values) {
+        int e = 0;
+        for (int i = 0; i < c; i++) {
+            for (int var : clauses[i]) {
+                if (var < 0) {
+                    if (!values[-var - 1]) {
+                        e++; break;
+                    }
+                } else {
+                    if (values[var - 1]) {
+                        e++; break;
+                    }
+                }
+            }
+        }
+        return e;
+    }
+
+    inline int testFlip(int i, bool* solution) {
+        solution[i] = !solution[i];
+        int e = evaluate(solution);
+        solution[i] = !solution[i];
+        return e;
+    }
 };
+
+int gsat(Formula& formula, bool* solution) {
+    int best = 0;
+    int vbest = formula.testFlip(0, solution);
+    for (int i = 1; i < formula.v; i++) {
+        int vi = formula.testFlip(i, solution);
+        if (vi > vbest) {
+            best = i;
+            vbest = vi;
+        }
+    }
+    solution[best] = !solution[best];
+    return vbest;
+}
+
+int test(Formula& formula) {
+    bool solution[formula.v];
+    for (int i = 0; i < formula.v; i++) {
+        solution[i] = rand() % 2;
+    }
+    int current = formula.evaluate(solution);
+    cout << "initial solution: " << current << endl;
+    while (true) {
+        int e = gsat(formula, solution);
+        if (e > current)
+            current = e;
+        else
+            break;
+    };
+    cout << "final solution: " << current << endl;
+    return current;
+}
 
 void readFormula(Formula& f, istream& stream) {
     string s;
@@ -44,13 +104,16 @@ void readFormula(Formula& f, istream& stream) {
 }
 
 int main() {
+    srand(time(NULL));
+
     ifstream file("flat75-1.cnf");
     if (!file) {
         cerr << "Unable to open file";
         return -1;
     }
-    Formula f;
-    readFormula(f, file);
+    Formula formula;
+    readFormula(formula, file);
     file.close();
+    test(formula);
     return 0;
 }
