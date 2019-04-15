a5b75cae6a1f4e63def5941df9650f4e46f5e8ec
Luísa Rocha <luisa.ras97@gmail.com>
2019-04-14 22:22:22 -0300

    bugfix in runSearch

--- MST/main.cpp
+++ MST/main.cpp
@@ -19,7 +19,8 @@ void runSearch(Graph& graph, Result& result) {
 	int* tree;
 
 	// First improvement
-
+	tree = graph.initializeTree();
+	graph.initializeIndexes(tree, indexes);
 	t1 = high_resolution_clock::now();
 	while(firstImprovement(graph, tree, indexes)) result.first_its++;
 	t2 = high_resolution_clock::now();
@@ -27,7 +28,6 @@ void runSearch(Graph& graph, Result& result) {
 	delete[] tree;
 
 	// Best improvement
-
 	tree = graph.initializeTree();
 	graph.initializeIndexes(tree, indexes);
 	t1 = high_resolution_clock::now();
@@ -56,45 +56,36 @@ void findPaths(Graph& graph, int i) {
 
 void test(int n, double p) {
 	Result result;
-
 	cout << "n = " << n << ", p = " << p << endl;
-
 	for (int i = 0; i < SAMPLES; i++) {
-
 		Graph graph(n, p);
-
-		//runSearch(graph, result);
-		findPaths(graph, i);
-
-    
+		runSearch(graph, result);
+		//findPaths(graph, i);
 	}
-	/*
 	result.first_its /= SAMPLES;
 	result.best_its /= SAMPLES;
 	result.first_time /= SAMPLES;
 	result.best_time /= SAMPLES;
-    
     cout << "First Improvement: " << result.first_time << " ms, " << result.first_its << " iterations." << endl;
-    cout << "Best Improvement: " << result.best_time << " ms, " << result.best_its << " iterations." << endl;*/
+    cout << "Best Improvement: " << result.best_time << " ms, " << result.best_its << " iterations." << endl;
     cout << endl;
-
 }
 
 int main() {
     srand(0);
-    test(8, 0.1);
-/*
-	test(100, 0.1);
-	test(100, 0.2);
-	test(100, 0.3);
+//    test(8, 0.1);
 
-	test(250, 0.05);
-	test(250, 0.1);
-	test(250, 0.2);
+	//test(100, 0.1);
+	//test(100, 0.2);
+	//test(100, 0.3);
 
-	test(500, 0.05);
-	test(500, 0.1);
-	test(500, 0.2);*/
+	//test(250, 0.05);
+	//test(250, 0.1);
+	//test(250, 0.2);
+
+	//test(500, 0.05);
+	//test(500, 0.1);
+	test(500, 0.2);
 
     return 0;
 }
