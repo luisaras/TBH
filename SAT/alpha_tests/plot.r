e6ee72ccfae169291d9c63d10562772ff7901ee4
luisaras <luisa.ras97@gmail.com>
2019-05-14 17:31:42 -0300

    new plot.r

--- SAT/alpha_tests/plot.r
+++ SAT/alpha_tests/plot.r
@@ -1,20 +1,18 @@
 
-for (file in c("greedy/par8-5-c", "greedy/flat50-1", "greedy/flat75-1", "greedy/flat100-1")) {
-	for (k in 0:5) {
-		name = paste(file, k, sep = " ")
-		d <- read.table(paste(name, "txt", sep = "."))
-		png(filename = paste(name, "png", sep = "."))
-		hist(d$V5)
-		dev.off()
+p <- function(folder){
+
+	for (file in c("par8-5-c", "flat50-1", "flat75-1", "flat100-1")) {
+		for (k in 0:5) {
+			path = paste(folder, paste(file, k), sep = "/")
+			title = paste(file, k, sep = ", k=")
+			d <- read.table(paste(path, "txt", sep = "."))
+			png(filename = paste(path, "png", sep = "."))
+			hist(d$V5, xlab = "", ylab = "", main = title)
+			dev.off()
+		}
 	}
+
 }
 
-for (file in c("grasp/par8-5-c", "grasp/flat50-1", "grasp/flat75-1", "grasp/flat100-1")) {
-	for (k in 0:5) {
-		name = paste(file, k, sep = " ")
-		d <- read.table(paste(name, "txt", sep = "."))
-		png(filename = paste(name, "png", sep = "."))
-		hist(d$V5)
-		dev.off()
-	}
-}
+p("greedy")
+p("grasp")
